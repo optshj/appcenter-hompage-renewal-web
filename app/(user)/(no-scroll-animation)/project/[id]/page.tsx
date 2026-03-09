583f12eb1e3554c67051a3b6efa01943e4b13d83
optshj <optshj@gmail.com>
2026-03-09 19:14:02 +0900

    🔍️ [Fix] 프로젝트 상세 페이지 메타데이터 수정

--- app/(user)/(no-scroll-animation)/project/[id]/page.tsx
+++ app/(user)/(no-scroll-animation)/project/[id]/page.tsx
@@ -1,3 +1,3 @@
 import { ProjectDetailPage } from 'pages/project';
 export default ProjectDetailPage;
-export { generateProjectListMetadata as generateMetadata } from 'pages/project';
+export { generateProjectDetailMetadata as generateMetadata } from 'pages/project';
